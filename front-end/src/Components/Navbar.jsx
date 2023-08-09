f4adef374717617302dfe4be64e830a8721a4317
GaelCruz <108242626+GaelCruz@users.noreply.github.com>
2023-08-08 23:48:06 -0500

    updated contact page

--- front-end/src/Components/Navbar.jsx
+++ front-end/src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <nav className="bg-sky-300 p-5 sm:p-8 shadow-lg">
+    <nav className="z-40 fixed bg-sky-300/90 p-5 sm:p-8 shadow-xl w-full">
       <div className="max-w-7xl mx-auto flex items-center justify-between">
         <h1 className="text-xl sm:trext-2xl">JE Smart Tech</h1>
         <div className="space-x-8 flex text-md space-x-3 hidden sm:block text-teal-700 self-center">
--- front-end/src/views/Contact.jsx
+++ front-end/src/views/Contact.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import ContactForm from '../Components/ContactForm'
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div>
+      <div className='text-5xl p-4 mt-'>
+        <p className='mt-24'>Contact Us</p>
+      </div>
+      <ContactForm />
+    </div>
   )
 }
