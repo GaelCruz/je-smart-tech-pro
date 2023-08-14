64c0acd0729c5f74c382ee9600ba4ee795c98fce
GaelCruz <108242626+GaelCruz@users.noreply.github.com>
2023-08-14 00:49:37 -0500

    updated Cam&Sec page

--- front-end/src/Components/ContactForm.jsx
+++ front-end/src/Components/ContactForm.jsx
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import '../css-files/Cam&Sec.css'
 
 export const ContactForm = () => {
   const form = useRef();
--- front-end/src/views/CamAndSec.jsx
+++ front-end/src/views/CamAndSec.jsx
@@ -15,29 +15,21 @@ export default function CamAndSec() {
       { "id": 3, 
       "title": "Title 1", 
       "desc": "Lörem ipsum befur nott mingar nysal. Homoten tira pas om senat, geore nen. Sulagen vanuda kvasinde." },
-      { "id": 4, 
-      "title": "Title 1", 
-      "desc": "Lörem ipsum befur nott mingar nysal. Homoten tira pas om senat, geore nen. Sulagen vanuda kvasinde." },
-      { "id": 5, 
-      "title": "Title 1", 
-      "desc": "Lörem ipsum befur nott mingar nysal. Homoten tira pas om senat, geore nen. Sulagen vanuda kvasinde." },
-      { "id": 6, 
-      "title": "Title 6", 
-      "desc": "Lörem ipsum befur nott mingar nysal. Homoten tira pas om senat, geore nen. Sulagen vanuda kvasinde." },
+      
   ]);
 
   return (
     <div>
-      <div className='pt-28'>
-        <h1 className='text-center p-20 text-4xl'>Cameras and Security</h1>
+      <div className="cam-sec-banner">
+        <h1 className='pt-44 font-bold text-white text-center p-20 text-4xl'>Cameras and Security</h1>
       </div>
       <div className='p-3 bg-gray-100'>
         {items.map((item, index) => (
-          <div key={index} className={`bg-[url(${img})] bg-cover mb-3 p-10 py-20 flex ${item.id % 2 === 0 ? 'flex-row-reverse': ''}`}>
+          <div key={index} className={`bg-${item.id} mb-3 p-10 py-20 flex ${item.id % 2 === 0 ? 'flex-row-reverse': ''}`}>
             <div className='bg-sky-300/50 max-w-lg p-3 shadow-2xl'>
               <h2 className='text-lg font-bold mb-2'>{item.title}</h2>
               <p className='mb-3'>{item.desc.slice(0,300)}...</p>
-              <Link className='text-sky-600' to={`/service/item/${item.id}`}>Read More...</Link>
+              <Link className='text-teal-300' to={`/service/item/${item.id}`}>Read More...</Link>
             </div>
           </div>
         ))}
