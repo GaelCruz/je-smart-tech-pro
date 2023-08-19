78acc5ae1b6dc82518d575fc454df5535fdb244a
GaelCruz <108242626+GaelCruz@users.noreply.github.com>
2023-08-19 16:27:32 -0500

    updated navbar

--- front-end/src/Components/Navbar.jsx
+++ front-end/src/Components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import jestLogo from '../assets/homepage/logo.png';
-import '../css-files/navbar.css'
 
 export default function Navbar() {
   const [isNavOpen, setIsNavOpen] = useState(false);
